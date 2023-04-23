816a018fa5e0811ef1043e92672ccb4f89260508
Scott Ames <scott@ames.sh>
2023-04-22 20:20:27 -0700

    feat: wezterm config

---
+++ home/private_dot_config/wezterm/keymaps.lua
@@ -0,0 +1,93 @@
+local w = require("wezterm")
+local action = w.action
+local direction_keys = {
+  Left = "h",
+  Down = "j",
+  Up = "k",
+  Right = "l",
+  -- reverse lookup
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "CTRL|SHIFT" or "ALT|SHIFT",
+    action = w.action_callback(function(win, pane)
+      if resize_or_move == "resize" then
+        win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+      else
+        win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+      end
+    end),
+  }
+end
+
+return {
+  leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 },
+  keys = {
+    -- move between split panes
+    split_nav("move", "h"),
+    split_nav("move", "j"),
+    split_nav("move", "k"),
+    split_nav("move", "l"),
+    -- resize panes
+    split_nav("resize", "h"),
+    split_nav("resize", "j"),
+    split_nav("resize", "k"),
+    split_nav("resize", "l"),
+
+    {
+      key = "n",
+      mods = "LEADER",
+      action = action.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+      key = "\\",
+      mods = "LEADER",
+      action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+      key = "|",
+      mods = "LEADER",
+      action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+      key = "-",
+      mods = "LEADER",
+      action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
+    {
+      key = "t",
+      mods = "LEADER",
+      action = action.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+      key = "[",
+      mods = "LEADER",
+      action = action.ActivateTabRelative(-1),
+    },
+    {
+      key = "]",
+      mods = "LEADER",
+      action = action.ActivateTabRelative(1),
+    },
+    -- Detach from current domain
+    {
+      key = "D",
+      mods = "LEADER",
+      action = action.DetachDomain("CurrentPaneDomain"),
+    },
+    { key = "l", mods = "LEADER", action = action.ShowLauncher },
+    { key = "n", mods = "LEADER", action = action.SpawnWindow },
+    { key = "-", mods = "LEADER", action = action.DecreaseFontSize },
+    { key = "0", mods = "LEADER", action = action.ResetFontSize },
+    { key = "=", mods = "LEADER", action = action.IncreaseFontSize },
+    { key = "+", mods = "LEADER", action = action.IncreaseFontSize },
+    { key = "y", mods = "LEADER", action = action.CopyTo("Clipboard") },
+    { key = "p", mods = "LEADER", action = action.PasteFrom("PrimarySelection") },
+  },
+}
---
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local k = require("keymaps")
+local w = require("wezterm")
+
+local c = {}
+
+if w.config_builder then
+  c = w.config_builder()
+end
+
+c.color_scheme = "Catppuccin Mocha"
+c.default_cursor_style = "BlinkingBar"
+c.font = w.font("FiraCode Nerd Font Mono")
+c.font_size = 14.0
+c.hide_tab_bar_if_only_one_tab = true
+c.leader = k.leader
+c.keys = k.keys
+c.tab_bar_at_bottom = true
+c.window_decorations = "RESIZE"
+c.window_background_opacity = 0.90
+c.window_close_confirmation = "AlwaysPrompt"
+
+return c
