2aa968380a563e972d391e92520cf34111edb6ef
Scott Ames <scott@ames.sh>
2023-05-09 18:12:14 -0700

    feat(chezmoi): handle init w/o fish

---
+++ home/.chezmoiscripts/run_once_after_00-always.sh
@@ -0,0 +1,12 @@
+#!/bin/env sh
+
+yellow='\033[0;33m'
+clear='\033[0m'
+
+warn() {
+  printf "${yellow}❕%s${clear}\n" "${@}"
+}
+
+if [ ! -x "$(command -v fish)" ]; then
+  warn "fish not found."
+fi
