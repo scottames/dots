ff47cb07a2de4f01c9fcc586274fe231652f0e8a
Scott Ames <scott@ames.sh>
2023-07-13 17:54:56 -0700

    fix: cleanup from distrobox additions + handy fish func

---
+++ home/private_dot_config/fish/custom_functions.d/fish_command_not_found.fish
@@ -0,0 +1,9 @@
+#!/bin/env fish
+
+function fish_command_not_found
+    if test -e /run/.containerenv -o -e /.dockerenv
+        distrobox-host-exec $argv
+    else
+        __fish_default_command_not_found_handler $argv
+    end
+end
