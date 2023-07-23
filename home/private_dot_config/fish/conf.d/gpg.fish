d99556839a81da961a08154ccf921c9b78ea6201
Scott Ames <scott@ames.sh>
2023-07-22 19:52:48 -0700

    fix: chezmoi gext + gpg init

--- home/private_dot_config/fish/conf.d/gpg.fish
+++ home/private_dot_config/fish/conf.d/gpg.fish
@@ -8,9 +8,9 @@ if test -f "$HOME/.gnupg/gpg-agent.conf"
   set -e SSH_AUTH_SOCK
   # Ensure that GPG Agent is used as the SSH agent
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-  if [ $DISTRO = "arch" ]
-    set -x GPG_TTY (tty)
-    # Refresh gpg-agent tty in case user switches into an X session
+  set -x GPG_TTY (tty)
+  if type -q gpg-connect-agent
+    # Refresh gpg-agent tty
     gpg-connect-agent updatestartuptty /bye >/dev/null
   end
 end
