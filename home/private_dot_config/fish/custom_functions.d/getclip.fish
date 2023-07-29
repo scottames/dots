a9049d2c40ae722ea4bd3959ab5a37b0b1951f96
Scott Ames <scott@ames.sh>
2023-07-28 19:52:41 -0700

    feat(fish): set/getclip support wayland

--- home/private_dot_config/fish/custom_functions.d/getclip.fish
+++ home/private_dot_config/fish/custom_functions.d/getclip.fish
@@ -1,7 +1,20 @@
 #!/bin/env fish
 
 function getclip --wraps xclip
-  if [ $HAS_XCLIP ]
-    xclip -selection c -o $argv
+  if $IS_LINUX
+    set -l login_session_type (loginctl show-session (loginctl | grep (whoami) | awk '{print $1}') -p Type)
+    set -l login_session (string split = $login_session_type)[2]
+
+    if [ $login_session = "wayland" ]
+      wl-paste $argv
+    else if [ $login_session = "x11" ]
+      xclip -selection c -o $argv
+    else
+      echo "No clipboard handler found."
+    end
+  else if $IS_MAC
+    pbpaste
+  else
+      echo "Only Linux & Mac supported."
   end
 end
--- home/private_dot_config/fish/custom_functions.d/setclip.fish
+++ home/private_dot_config/fish/custom_functions.d/setclip.fish
@@ -1,9 +1,20 @@
 #!/bin/env fish
 
 function setclip --description "Set to clipboard"
-  if [ $HAS_XCLIP ]
-    xclip -selection c $argv
-  else if [ $IS_MAC ]
+  if $IS_LINUX
+    set -l login_session_type (loginctl show-session (loginctl | grep (whoami) | awk '{print $1}') -p Type)
+    set -l login_session (string split = $login_session_type)[2]
+
+    if [ $login_session = "wayland" ]
+      wl-copy $argv
+    else if [ $login_session = "x11" ]
+      xclip -selection c $argv
+    else
+      echo "No clipboard handler found."
+    end
+  else if $IS_MAC
     pbcopy $argv
+  else
+      echo "Only Linux & Mac supported."
   end
 end
