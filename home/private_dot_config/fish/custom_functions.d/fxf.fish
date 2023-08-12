540a519c12ceb902738714b6a9786dd6022e95f8
Scott Ames <scott@ames.sh>
2023-08-12 11:45:18 -0700

    refactor: fish(fx -> fxf) + aqua(add fx)

--- home/private_dot_config/fish/custom_functions.d/fxf.fish
+++ home/private_dot_config/fish/custom_functions.d/fxf.fish
@@ -1,6 +1,6 @@
 #!/bin/env fish
 
-function fx --description "Search functions + execute selsected"
+function fxf --description "Search functions + execute selsected"
   set _cmd ( functions | fzf --preview 'fish -c "functions {}"' )
   eval $_cmd
 end
