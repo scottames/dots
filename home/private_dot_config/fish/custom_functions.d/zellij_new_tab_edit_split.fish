22de9aa534c1a4efa41d17fec5eb23bd747ce65d
Scott Ames <scott@ames.sh>
2024-07-02 20:02:44 -0700

    refactor(zellij): new tab func layout

--- home/private_dot_config/fish/custom_functions.d/zellij_new_tab_edit_split.fish
+++ home/private_dot_config/fish/custom_functions.d/zellij_new_tab_edit_split.fish
@@ -1,24 +1,29 @@
 #!/usr/bin/env fish
 
 function zellij_new_tab_edit_split \
-    --description "Edit dir/file in new Zellij tab. Defaults to horizonal layout, can specify --vertical."
+    --description "Edit dir/file in new Zellij tab. Defaults to horizontal layout, can specify --vertical."
 
-    set -l _argparse_options h/horizontal v/vertical 'n/name=?' 'l/layout=?'
+    set -l _argparse_options h/horizontal v/vertical 's/suspended=?' 'n/name=?' 'l/layout=?'
     argparse -n ze $_argparse_options -- $argv
 
     set -l _DIR $HOME
     set -l _TAB_NAME "~"
     set -l _NVIM_EXTRA_ARGS ""
     set -l _ARGS ""
-    set -l _SPLIT_DIRECTION horizontal
+    set -l _SPLIT_DIRECTION vertical
+    set -l _START_SUSPENDED true
     set _LAYOUT_FILE ""
 
     function __cleanup
         functions -e __cleanup
     end
 
-    if set -q _flag_vertical
-        set _SPLIT_DIRECTION vertical
+    if set -q _flag_horizontal
+        set _SPLIT_DIRECTION horizontal
+    end
+
+    if set -q _flag_suspended
+        set _START_SUSPENDED $_flag_suspended
     end
 
     if test (count $argv) -gt 0
@@ -51,8 +56,11 @@ function zellij_new_tab_edit_split \
   layout {
       tab {
           pane split_direction=\"$_SPLIT_DIRECTION\" {
-              pane command=\"nvim\"$_ARGS
-              pane
+              pane command=\"nvim\"$_ARGS start_suspended=$_START_SUSPENDED
+              pane stacked=true {
+                pane
+                pane
+              }
           }
           pane size=1 borderless=true {
               plugin location=\"zellij:compact-bar\"
