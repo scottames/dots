a61d3c5c01ebab14108dfd93dc9b5e8546f84bea
Scott Ames <scott@ames.sh>
2025-05-29 08:47:35 -0700

    feat(distrobox/f): init script
    Add init script to install some bits on startup (smaller image, easier
    to enable/disable)

---
+++ home/private_dot_config/distrobox/scripts/executable_init_f.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+
+function install_vscode
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+
+    dnf check-update
+    sudo dnf install -y code
+end
+
+function install_claude_code
+    sudo npm install -g @anthropic-ai/claude-code
+end
+
+function install_openai_codex
+    sudo npm install -g @openai/codex
+end
+
+
+install_vscode
+install_claude_code
+install_openai_codex
