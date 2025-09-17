63fbdee3e0004f73ce789094ac01608852957370
Scott Ames <scott@ames.sh>
2025-09-17 09:20:05 -0700

    fix(fish): github_token_load() only if interactive

--- home/private_dot_config/fish/custom_functions.d/github_token_load.fish
+++ home/private_dot_config/fish/custom_functions.d/github_token_load.fish
@@ -3,7 +3,11 @@
 function github_token_load \
     --description "(optionally) Load GITHUB_TOKEN into the environment using ghtkn"
 
-    $GUM_BIN confirm "Load GITHUB_TOKEN with ghtkn?" \
-        --default=false --timeout=10s \
-        && set -gx GITHUB_TOKEN (ghtkn get)
+    if status --is-interactive
+        $GUM_BIN confirm "Load GITHUB_TOKEN with ghtkn?" \
+            --default=false --timeout=10s \
+            && set -gx GITHUB_TOKEN (ghtkn get)
+    else
+        printf_info "in non-interactive shell, skipping ghtkn prompt"
+    end
 end
