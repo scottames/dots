80f7e6137ca7e5cce808015e746c2e58f3498456
Scott Ames <scott@ames.sh>
2025-10-22 15:50:17 -0700

    fix(nvim/ai): add missing sidekick keys

--- home/private_dot_config/nvim/lua/plugins/ai.lua
+++ home/private_dot_config/nvim/lua/plugins/ai.lua
@@ -45,9 +45,30 @@ return {
     },
     keys = {
       -- stylua: ignore
+    {
+      "<tab>",
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require("sidekick").nes_jump_or_apply() then
+          return "<Tab>" -- fallback to normal tab
+        end
+      end,
+      expr = true,
+      desc = "Goto/Apply Next Edit Suggestion",
+    },
+      {
+        "<c-.>",
+        function()
+          require("sidekick.cli").toggle()
+        end,
+        desc = "Sidekick Toggle",
+        mode = { "n", "t", "i", "x" },
+      },
       {
         "<leader>aa",
-        function() require("sidekick.cli").toggle() end,
+        function()
+          require("sidekick.cli").toggle()
+        end,
         desc = "Sidekick: Toggle CLI",
       },
       {
