251cf7f1b496ea0b308df45ca6453a78605e438d
Scott Ames <scott@ames.sh>
2026-01-02 14:19:55 -0800

    chore: a little pruning

--- home/private_dot_config/distrobox/scripts/executable_init_f.fish
+++ home/private_dot_config/distrobox/scripts/executable_init_f.fish
@@ -13,35 +13,4 @@ function fix_npm_permissions
     sudo chown -R $USER:$USER "$HOME/.npm-global/"
 end
 
-function install_npms
-
-    if command -v npm
-        set -l npm_registry_ping "https://registry.npmjs.org/-/ping?write=true"
-        if ! curl --fail --silent --show-error --connect-timeout 5 $npm_registry_ping >/dev/null
-            print_warn "npm ping failed. Skipping installing npm packages."
-            return 0
-        end
-    else
-        print_warn "no npm found. Skipping installing npm packages."
-        return 0
-    end
-
-    function npm_install
-        npm install -g $argv
-
-    end
-
-    set npm_packages_to_install \
-        @anthropic-ai/claude-code \
-        @google/gemini-cli \
-        @openai/codex
-
-    for pkg in $npm_packages_to_install
-        print_info "npm install $pkg"
-        npm_install $pkg
-    end
-
-end
-
 fix_npm_permissions
-install_npms
