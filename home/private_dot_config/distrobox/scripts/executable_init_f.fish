67987aeeb2baf2df6b73939e66aa6d1322e9df13
Scott Ames <scott@ames.sh>
2026-01-27 09:53:16 -0800

    refactor(distrobox/f): dry up init symlinks

--- home/private_dot_config/distrobox/scripts/executable_init_f.fish
+++ home/private_dot_config/distrobox/scripts/executable_init_f.fish
@@ -13,4 +13,31 @@ function fix_npm_permissions
     sudo chown -R $USER:$USER "$HOME/.npm-global/"
 end
 
+set -l host_exec_commands \
+    docker \
+    flatpak \
+    hostnamectl \
+    loginctl \
+    niri \
+    nmcli \
+    op \
+    podman \
+    reboot \
+    rpm-ostree \
+    systemctl \
+    transactional-update \
+    ugum \
+    ujust \
+    ydotool \
+    xclip \
+    xdg-open
+
+function create_host_exec_symlinks
+    for cmd in $argv
+        ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/$cmd
+    end
+end
+
+create_host_exec_symlinks $host_exec_commands
+
 fix_npm_permissions
