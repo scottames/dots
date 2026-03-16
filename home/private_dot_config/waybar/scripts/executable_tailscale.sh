e5fc363a5f1b6ef92cb6a7bee3b3fe80e7b6401e
Scott Ames <scott@ames.sh>
2026-03-16 13:28:38 -0700

    feat(waybar): cleanup + add proper disabled option

--- home/private_dot_config/waybar/scripts/executable_tailscale.sh
+++ home/private_dot_config/waybar/scripts/executable_tailscale.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ -f "${HOME}/.config/waybar/.no-tailscale" ]]; then
+  echo '{"text": " ", "class": "disabled", "tooltip": "tailscale disabled"}'
+  exit 0
+fi
+
 if tailscale status &>/dev/null; then
   if tailscale exit-node list | grep -q selected; then
     tooltip=$(tailscale exit-node list | grep selected | awk '{print $2" "$3}')
     echo "{\"text\": \" \", \"class\": \"enabled\", \"tooltip\": \"${tooltip}\"}"
   else
     tooltip="No exit-node assigned"
-    echo "{\"text\": \" \", \"class\": \"disabled\", \"tooltip\": \"${tooltip}\"}"
+    echo "{\"text\": \" \", \"class\": \"no-exit-node\", \"tooltip\": \"${tooltip}\"}"
   fi
 else
   tooltip="Tailscale is not connected"
-  echo "{\"text\": \" \", \"class\": \"disabled\", \"tooltip\": \"${tooltip}\"}"
+  echo "{\"text\": \" \", \"class\": \"disconnected\", \"tooltip\": \"${tooltip}\"}"
 fi
