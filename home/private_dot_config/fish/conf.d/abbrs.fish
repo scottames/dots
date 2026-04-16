60c5a969395db1ecdaebdb8342179b4317130a6a
Scott Ames <scott@ames.sh>
2026-04-16 15:58:27 -0700

    feat: nono + opencode (needs work)

--- home/private_dot_config/fish/conf.d/abbrs.fish
+++ home/private_dot_config/fish/conf.d/abbrs.fish
@@ -157,9 +157,9 @@ abbr -a nmap_slow "sudo nmap -sS -v -T1"
 abbr -a nmap_traceroute "sudo nmap -sP -PE -PS22,25,80 -PA21,23,80,3389 -PU -PO --traceroute "
 abbr -a nmap_web_safe_osscan "sudo nmap -p 80,443 -O -v --osscan-guess --fuzzy "
 abbr -a nosleep "systemd-inhibit --what=handle-lid-switch sleep 2592000" # disable systemd sleep
-abbr -a nonoc "nono run --detached --rollback --profile claude-code --allow-cwd -- claude"
-abbr -a nonoo "nono run --detached --rollback --profile opencode-local --allow-cwd -- opencode"
-abbr -a nonooc "nono run --detached --rollback --profile opencode-local --allow-cwd -- opencode --continue"
+abbr -a nonoc "nono run --profile claude-code --allow-cwd -- claude"
+abbr -a nonoo "nono run --profile opencode-local --allow-cwd -- opencode"
+abbr -a nonooc "nono run --profile opencode-local --allow-cwd -- opencode --continue"
 abbr -a o open
 abbr -a oc opencode
 abbr -a occ "opencode --continue"
