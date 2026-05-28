126ffc0e9fd67f2bb04fa7f4a133656ccc4614f5
Scott Ames <scott@ames.sh>
2026-05-28 08:47:34 -0700

    chore(zen): migrate keybinds (+dyff helper)

--- home/private_dot_config/fish/conf.d/abbrs.fish
+++ home/private_dot_config/fish/conf.d/abbrs.fish
@@ -226,6 +226,7 @@ abbr -a ze. "zellij_new_tab $DOTS --name='.'" # see functions
 abbr -a zect "zellij_new_tab $MY_SRC_DIR/containers/main"
 abbr -a zeag "zellij_new_tab $MY_SRC_DIR/.agents/main"
 abbr -a zecp "zellij_new_tab $MY_SRC_DIR/copr/main"
+abbr -a zenkbd "dyff between $HOME/.zen/default-release/zen-keyboard-shortcuts.json $DOTS/home/private_dot_zen/default-release/zen-keyboard-shortcuts.json"
 abbr -a zeo "zellij_new_tab $HOME/.obsidian/this --name='obsidian'"
 abbr -a zet zellij_new_tab
 abbr -a zego "zellij_new_tab --layout=go"
